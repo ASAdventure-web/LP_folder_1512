46d70c0ea275d5e749223c6bd464c2d40c905645
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-11-27 12:09:17 +0100

    foto's en database

--- folder/application/views/includes/foto.php
+++ folder/application/views/includes/foto.php
@@ -2,8 +2,12 @@
 	<div class="col-6 row-2 image-container">
 		<?php if ($photos[$photocount]['link'] != NULL) { ?>
 			<a href="http://www.asadventure.com<?php echo $photos[$photocount]['link'] ?>">
-		<?php } ?>
-			<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld'] ?>">
+		<?php } 
+			if ($photos[$photocount]['taal'] != NULL) { ?>
+				<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld']?>_<?php echo $lang ?>.jpg">
+			<?php } else { ?>
+				<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld']?>.jpg">
+			<?php } ?>
 		<?php if ($photos[$photocount]['link'] != NULL) { ?>
 			</a>
 		<?php } ?>
@@ -13,8 +17,12 @@
 	<div class="col-6 row-2 image-container right">
 		<?php if ($photos[$photocount]['link'] != NULL) { ?>
 			<a href="http://www.asadventure.com<?php echo $photos[$photocount]['link'] ?>">
-		<?php } ?>
-				<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld'] ?>">
+		<?php } 
+			if ($photos[$photocount]['taal'] != NULL) { ?>
+				<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld']?>_<?php echo $lang ?>.jpg">
+			<?php } else { ?>
+				<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld']?>.jpg">
+			<?php } ?>
 		<?php if ($photos[$photocount]['link'] != NULL) { ?>
 			</a>
 		<?php } ?>
