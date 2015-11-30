2977d5afc7270b055168e665816e05a2586fae8d
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-11-30 08:50:31 +0100

    header afwerken

--- folder/application/views/includes/videowrapper.php
+++ folder/application/views/includes/videowrapper.php
@@ -1,7 +1,23 @@
-<div class="video-wrapper">
+<!-- "<div class="video-wrapper">
 	<?php if($lang == 'nl' or $lang == 'en') { ?>
 <iframe src="https://www.youtube.com/embed/Gm5rpUZnhkE?rel=0&showinfo=0&controls=0&hd=1&autohide=1" frameborder="0"></iframe>
 		<?php } else { ?>
 <iframe src="https://www.youtube.com/embed/VWYtR18lrgo?rel=0&showinfo=0&controls=0&hd=1&autohide=1" frameborder="0"></iframe>
 		<?php } ?>
-		</div>
+		</div>-->
+
+<div class="gif">
+	<img class="gif-kerst" src="http://static.asadventure.com/includes/banner/category-page/151202_kerstfolder/GIF_ANIMATED_v8.gif" style="width:100%; height:100%;">
+</div>
+<div class="title-container">
+	<div class="title">
+		<?php if ($lang == 'nl'){?>
+			<img src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/tussentitels/titel.png">
+		<?php } elseif ($lang == 'fr'){ ?>
+			<p class="head">Rendez votre hiver mémorable</p>
+			<p class="sub">des cadeaux qie vont vous emballer</p>
+		<?php } else {?>
+			<p class="head">Make your winter Memorable</p>
+		<?php } ?>
+	</div>
+	</div>
