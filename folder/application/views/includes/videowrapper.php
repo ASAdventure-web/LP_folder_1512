955c103d5b76b3e49e2810dbaa2a5bd5e3997557
Olivier Duchesne <olivier.duchesne@asadventure.com>
2015-12-01 18:11:56 +0100

    aanpassen overlay FR filmpje

--- folder/application/views/includes/videowrapper.php
+++ folder/application/views/includes/videowrapper.php
@@ -17,7 +17,7 @@
 			<p class="sub">cadeautips om mee uit te pakken</p>
 		<?php } elseif ($lang == 'fr'){ ?>
 			<p class="head">Rendez votre hiver<br><span><img class="golf" src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/overlay/golf.png"></span> m&eacute;morable <span><img class="golf" src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/overlay/golf.png"></span></p>
-			<p class="sub">des cadeaux qie vont vous emballer</p>
+			<p class="sub">des cadeaux qui vont vous emballer</p>
 		<?php } else {?>
 			<p class="head">Make your winter<br><span><img class="golf" src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/overlay/golf.png"></span> memorable <span><img class="golf" src="http://static.asadventure.com/includes/landingpage/2015_kerstfolder/images/overlay/golf.png"></span></p>
 			<p class="sub">Gifts that take you by suprise</p>
