b8bf3e91c1e693627b254a6fd8ebeccab523a0c8
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-12-01 17:32:08 +0100

    link in andere talen toevoegen

--- folder/application/models/photo_model.php
+++ folder/application/models/photo_model.php
@@ -6,20 +6,20 @@ class Photo_model extends CI_Model {
 		$this->load->database();
 	}
 
-	public function get_photos($filter = FALSE)
+	public function get_photos($filter = FALSE, $lang = 'NL')
 	{
 		if ($filter === FALSE)
 		 {
-			$this->db->from('Fotos');
-			$this->db->order_by('hoofdcategorie','asc');
-			$query = $this->db->get();
+					$query = $this->db->query("SELECT * FROM Fotos WHERE (taal IS NULL OR taal='".$lang."') ORDER BY hoofdcategorie asc" );
 			return $query->result_array();
 		}
 		else {
-		$query = $this->db->query("SELECT * FROM Fotos WHERE ".$filter." IS NOT NULL ORDER BY $filter" );
+		$query = $this->db->query("SELECT * FROM Fotos WHERE ".$filter." IS NOT NULL AND (taal IS NULL OR taal='".$lang."') ORDER BY ".$filter );
 		return $query->result_array();
 
 		}
+
+
 
 	}
 
